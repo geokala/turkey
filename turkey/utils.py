64beb37cc2134de257287a6a0f1759c83ad8f00e
Madda <nobody.youknow@local>
2016-01-09 16:48:43 +0000

    Remove unused function

--- turkey/utils.py
+++ turkey/utils.py
@@ -9,22 +9,6 @@ def int_or_null(data):
         return int(data)
 
 
-def user_not_authorised_to(action):
-    """
-        This will flash a warning for the user that they're not allowed to
-        perform the action they just tried to perform.
-        It will then return a redirect to the home page.
-
-        Keyword arguments:
-        action -- The action the user is not allowed to perform.
-    """
-    flash(
-        'You are not authorised to %s!' % action,
-        'danger',
-    )
-    return redirect(url_for('home'))
-
-
 def get_goals(include_top_level=True):
     all_goals = {
         goal.id: {
